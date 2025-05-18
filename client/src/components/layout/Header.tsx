c6df92726a0def13769f8df30fd1a74eb7578736
freelanceos <freelanceos2025@gmail.com>
2025-05-18 22:40:10 +0300

    Connect to Supabase

--- client/src/components/layout/Header.tsx
+++ client/src/components/layout/Header.tsx
@@ -19,7 +19,7 @@ const Header = () => {
       <div className="container mx-auto px-4">
         <div className="flex items-center justify-between py-4 flex-row-reverse">
           {/* Logo on right */}
-          <Link href="/">
+          <Link href="pic/logo.png">
             <img
               src="src/pic/logo.png"
               alt="freelanceos logo"
